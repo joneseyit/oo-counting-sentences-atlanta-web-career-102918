e3ddfa6cc42968026653240990bc2db4379206e0
Andrew Jones <joneseyit@protonmail.com>
2018-11-01 18:42:16 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    (self.end_with?(".")) ? true : false
+
   end
 
   def question?
+    (self.end_with?("?")) ? true : false
 
   end
 
   def exclamation?
-
+      (self.end_with?("!")) ? true : false
   end
+
+
 
   def count_sentences
+    count = 0
+    punctuation = "!?."
+    words = self.split(" ")
+    words.each do|word|
+      
+      if punctuation.include?(word[word.length-1]) == true
+        count += 1
+      end
+    end
 
+    count
   end
+
 end
